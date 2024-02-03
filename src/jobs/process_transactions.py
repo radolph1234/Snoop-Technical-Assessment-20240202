807d589e974cc0309cda1fae9843bd961acb3469
Ross.Adolph <Ross.Adolph@redkite.com>
2024-02-03 16:50:48 +0000

    Finished Transform code

--- src/jobs/process_transactions.py
+++ src/jobs/process_transactions.py
@@ -1,10 +1,13 @@
 import os
 import json
-
+from datetime import datetime
 import pandas as pd
 
 import ConfigurationHelpers
 import DataReaders
+import TransformHelpers
+
+pd.set_option('display.max_columns', None)
 
 
 def process():
@@ -12,7 +15,9 @@ def process():
 
     transactions_df = extract(sourced_filepath)
 
-    transform(transactions_df)
+    customer_df, transactions_df, invalid_trans_df = transform(transactions_df)
+
+    load(customer_df, transactions_df, invalid_trans_df)
 
 
 def extract(sourced_filepath):
@@ -24,17 +29,60 @@ def extract(sourced_filepath):
     return pd.DataFrame.from_dict(transactions_dict['transactions'])
 
 
-def transform(transactions_df):
-    # Validate currency
-    # Check for inValid dates
+def transform(trans_df):
+    # Create transactions primary key
+    trans_df = TransformHelpers.add_unique_hashkey(trans_df, ['customerId', 'transactionId'])
+
+    # obscure PII data
+    trans_df = TransformHelpers.sha2_hash(trans_df, 'customerName')
+
+    # Perform DQ Checks
+    trans_df['validTransactionDate'] = trans_df['transactionDate'].apply(TransformHelpers.DQ_date)
+    trans_df['validSourceDate'] = trans_df['sourceDate'].apply(TransformHelpers.DQ_date)
+    trans_df['validCurrency'] = trans_df['currency'].apply(TransformHelpers.DQ_currency)
+
+    # Filter for valid rows
+    invalid_trans_df = trans_df[(trans_df['validTransactionDate'] == False) |
+                                (trans_df['validSourceDate'] == False) |
+                                (trans_df['validCurrency'] == False)]
+    
+    trans_df = trans_df[(trans_df['validTransactionDate'] == True) &
+                        (trans_df['validSourceDate'] == True) &
+                        (trans_df['validCurrency'] == True)]
+
+    # Cast datetime columns
+    trans_df = TransformHelpers.cast_datetime(trans_df, 'transactionDate', '%Y-%m-%d')
+    trans_df = TransformHelpers.cast_datetime(trans_df, 'sourceDate', '%Y-%m-%dT%H:%M:%S')
+    
     # Check for duplicate transactions records
-    transactions_df['rank'] = (
-        transactions_df
-        .groupby(['customerId', 'transactionId'])['sourceDate']
-        .rank(ascending=True)
+    trans_df = TransformHelpers.dense_rank_desc(df=trans_df,
+                                                groupby_cols=['UniqueHashKey'],
+                                                rank_col='sourceDate')
+
+    trans_df['duplicate'] = trans_df['rank'].apply(lambda x: True if x > 1.0 else False)
+    trans_df = trans_df.drop(columns=['rank'])
+
+    valid_trans_df = trans_df[trans_df['duplicate'] == False]
+    duplicates = trans_df[trans_df['duplicate'] == True]
+    
+    invalid_trans_df = pd.concat([invalid_trans_df, duplicates])
+    invalid_trans_df = invalid_trans_df.drop(columns=['customerName'])
+
+    # Create customer and transactions tables
+    valid_trans_df = TransformHelpers.dense_rank_desc(df=trans_df,
+                                                      groupby_cols=['customerId'],
+                                                      rank_col='transactionDate')
+
+    customer_df = (
+        valid_trans_df
+        .loc[valid_trans_df['rank'] == 1.0, ['customerId', 'customerName', 'transactionDate']]
+        .rename(columns={'transactionDate': 'lastTransaction'})
     )
-    pass
+
+    transactions_df = valid_trans_df.drop(columns=['customerName', 'rank'])
+
+    return customer_df, transactions_df, invalid_trans_df
 
 
-def load():
+def load(customer_df, transactions_df, invalid_trans_df):
     pass
---
+++ src/utilities/TransformHelpers.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+import hashlib
+
+
+def sha2_hash(df, column):
+    df[column] = df[column].apply(lambda x: hashlib.sha256(x.encode()).hexdigest())
+
+    return df
+
+
+def add_unique_hashkey(df, columns):
+    df['UniqueHashKey'] = df[columns].astype(str).apply(''.join, axis=1)
+    df = sha2_hash(df, 'UniqueHashKey')
+
+    return df
+
+
+def DQ_date(value):
+
+    format = '%Y-%m-%d' if len(value) == 10 else '%Y-%m-%dT%H:%M:%S'
+
+    try:
+        dt = datetime.strptime(value, format)
+        
+        if dt <= datetime.now():
+            return True
+        
+        return False
+    
+    except:
+        return False
+
+
+def DQ_currency(value):
+    if value in ['EUR', 'GBP', 'USD']:
+        return True
+    else:
+        return False
+    
+
+def cast_datetime(df, column, format):
+    df[column] = df[column].apply(lambda x: datetime.strptime(x, format))
+
+    return df
+
+
+def dense_rank_desc(df, groupby_cols, rank_col):
+    df['rank'] = (
+        df
+        .groupby(groupby_cols)[rank_col]
+        .rank(method='dense', ascending=False)
+    )
+    return df
